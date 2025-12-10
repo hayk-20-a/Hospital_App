a121a3c10bf05da6f09d542cb62f8a7a44a13640
hayk-20-a <hayk.arakelyann.7@gmail.com>
2025-12-10 23:12:42 +0400

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,12 +1,13 @@
 import { initializeApp } from "firebase/app";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "PASTE_YOURS",
-  authDomain: "PASTE_YOURS",
-  projectId: "PASTE_YOURS",
-  storageBucket: "PASTE_YOURS",
-  messagingSenderId: "PASTE_YOURS",
-  appId: "PASTE_YOURS"
+  apiKey: "AIzaSyDNKl_oaittxyrYahRK2x6M7VjZ1TZogys",
+  authDomain: "hospital-app-8d5a5.firebaseapp.com",
+  projectId: "hospital-app-8d5a5",
+  storageBucket: "hospital-app-8d5a5.firebasestorage.app",
+  messagingSenderId: "332760595556",
+  appId: "1:332760595556:web:f72cb484682a176d8d7371",
+  measurementId: "G-GHZJ3E5XV5"
 };
-
 export const app = initializeApp(firebaseConfig);
