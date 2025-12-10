bf04ffda71cb0496fedd0562526a7fa5fe4051e6
hayk-20-a <hayk.arakelyann.7@gmail.com>
2025-12-10 22:58:22 +0400

    Create firebase.ts

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "PASTE_YOURS",
+  authDomain: "PASTE_YOURS",
+  projectId: "PASTE_YOURS",
+  storageBucket: "PASTE_YOURS",
+  messagingSenderId: "PASTE_YOURS",
+  appId: "PASTE_YOURS"
+};
+
+export const app = initializeApp(firebaseConfig);
